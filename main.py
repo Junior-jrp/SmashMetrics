bac61b9c4a444af0f6a586af956faef6aa2d8224
Junior Pamplona <111400312+Junior-jrp@users.noreply.github.com>
2025-02-09 03:45:31 -0300

    Update main.py
    Adaptação do main com icone

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import sys
+import ctypes
 from PySide6.QtWidgets import QApplication
-from Telas import SmashMetricsUI
-from Funcionalidades import Funcionalidades
+from PySide6.QtGui import QIcon
+from core.telas import SmashMetricsUI
+from core.funcionalidades import Funcionalidades
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = SmashMetricsUI()
 
-    # Integrar funcionalidades
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("SmashMetrics")
+    app = QApplication(sys.argv)
+    icon_path = "logo_smashmetrics_removebg_preview_CD6_icon.ico"
+    app.setWindowIcon(QIcon(icon_path))
+
+    try:
+        with open("assets/styles.css", "r") as f:
+            app.setStyleSheet(f.read())
+    except FileNotFoundError:
+        print("⚠ Arquivo 'styles.css' não encontrado. O aplicativo usará o estilo padrão.")
+
+    window = SmashMetricsUI()
     window.funcionalidades = Funcionalidades()
+    window.setWindowIcon(QIcon(icon_path))
 
     window.show()
     sys.exit(app.exec())
